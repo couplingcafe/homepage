fa1dfaf097f93db6518315cb104d93b709e045a0
Kevo <ktngo09@gmail.com>
2024-02-07 14:05:56 -0800

    update homepage with store links and screenshots

--- src/js/index.js
+++ src/js/index.js
@@ -2,53 +2,25 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('Loaded');
   initAnalytics();
   initLoaded();
-  initSubscribeForm();
   initViewportAnimations();
 });
 
 function initAnalytics () {
   if (window.location.host.startsWith('localhost')) { return; }
 
-  if (window.location.pathname === '/') {
-    let ticking = false;
-    const scrollTracked = [];
-    window.addEventListener('scroll', evt => {
-      if (!ticking) {
-        window.requestAnimationFrame(() => {
-          const percent = this.scrollY / (document.body.clientHeight - window.innerHeight);
-          if (percent >= 0.25 && !scrollTracked[0]) {
-            analytics.track('Home Page: 25% Scroll Depth');
-            scrollTracked[0] = true;
-          }
-          if (percent >= 0.5 && !scrollTracked[1]) {
-            analytics.track('Home Page: 50% Scroll Depth');
-            scrollTracked[1] = true;
-          }
-          if (percent >= 0.75 && !scrollTracked[2]) {
-            analytics.track('Home Page: 75% Scroll Depth');
-            scrollTracked[2] = true;
-          }
-          if (percent >= 0.95 && !scrollTracked[3]) {
-            analytics.track('Home Page: 100% Scroll Depth');
-            scrollTracked[3] = true;
-          }
-          ticking = false;
-        });
-        ticking = true;
-      }
-    });
-  }
-
-  $$('form.subscribe button').forEach(btn => {
+  $$('button').forEach(btn => {
     btn.addEventListener('click', () => {
       console.log('Subscribe button click.');
-      analytics.track('Home Page: Subscribe');
+      analytics.track('Home Page: Button Click', {
+        id: btn.getAttribute('data-track') || btn.getAttribute('id')
+      });
     });
   });
 
   $$('a').forEach(link => {
     link.addEventListener('click', () => {
       analytics.track('Home Page: Link Click', {
+        id: link.getAttribute('data-track') || link.getAttribute('id'),
         href: link.getAttribute('href'),
         text: link.innerText
       });
@@ -92,63 +64,6 @@ function initLoaded () {
   }, 1000);
 }
 
-/**
- * Init XHR handler to subscribe.
- */
-function initSubscribeForm () {
-  const forms = $$('.subscribeForm');
-  if (!forms || !forms.length) { return; }
-
-  forms.forEach(form => {
-    const button = form.querySelector('.submit');
-    const input = form.querySelector('input[type="email"]');
-
-    let originalHeaderText = '';
-
-    button.addEventListener('click', evt => {
-      evt.preventDefault();
-
-      const xhr = new XMLHttpRequest();
-      let endpoint = 'http://localhost:5000/mail/subscribe';
-      if (process.env.NODE_ENV === 'production') {
-        endpoint = 'https://subscribe.learncoupling.com/mail/subscribe';
-      }
-      xhr.open('POST', endpoint);
-
-      xhr.addEventListener('load', () => {
-        if (button) {
-          button.disabled = true;
-          button.innerHTML = 'Subscribed!';
-        }
-      });
-
-      xhr.addEventListener('error', () => {
-        window.location.href = 'http://eepurl.com/hfV9gr';
-      });
-
-      xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-      xhr.send(JSON.stringify({
-        email: input.value,
-        source: document.title
-      }));
-
-      analytics.identify(null, {
-        email: input.value
-      });
-
-      return false;
-    });
-
-    if (button) {
-      input.addEventListener('keydown', () => {
-        if (button.hasAttribute('disabled')) {
-          button.innerHTML = 'Subscribe';
-          button.removeAttribute('disabled');
-        }
-      });
-    }
-  });
-}
 
 function initViewportAnimations() {
   const iconTextSections = $$('.iconText');
